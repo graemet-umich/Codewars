75dfdea239c860b5aaa56ea533afa5a22d9208d6
Graeme Tank <graemet@umich.edu>
2023-09-04 11:48:35 -0400

    simple pig latin src comments and test code

---
+++ Javascript/test/simplePigLatin.spec.js
@@ -0,0 +1,12 @@
+// const Test = require('@codewars/test-compat');
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold=0;
+const pigIt = require('../src/twoToOne').pigIt;
+
+describe("Tests", () => {
+  it("test", () => {
+    assert.equal(pigIt('Pig latin is cool'),'igPay atinlay siay oolcay')
+    assert.equal(pigIt('This is my string'),'hisTay siay ymay tringsay')
+  });
+});
